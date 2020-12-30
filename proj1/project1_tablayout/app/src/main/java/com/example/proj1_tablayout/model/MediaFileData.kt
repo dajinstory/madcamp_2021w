a64bcd4ab3b4523f68dcf7d6c590bbdd92a0ee1c
Jung In Rhee <ysrheee@kaist.ac.kr>
2020-12-30 23:04:35 +0900

    tablayout_1st_commit/with_galley_demo

---
+++ proj1/project1_tablayout/app/src/main/java/com/example/proj1_tablayout/model/MediaFileData.kt
@@ -0,0 +1,11 @@
+package com.example.proj1_tablayout.model
+
+import android.net.Uri
+import java.util.*
+
+data class MediaFileData(
+    val id:Long,
+    val dateTaken: Date,
+    val displayName: String,
+    val uri: Uri
+)
