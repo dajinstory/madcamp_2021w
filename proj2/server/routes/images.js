d2e282a372ee7383059b061aac8f7f33039dd11a
Dajin Han <dajinstory@naver.com>
2021-01-11 09:59:32 +0000

    image upload

--- proj2/server/routes/images.js
+++ proj2/server/routes/images.js
@@ -4,38 +4,54 @@ const http = require('http');
 const url = require('url');
 const fs = require('fs');
 
+
+// directory setting
+fs.readdir("images", (err) => {
+	if (err) {
+		console.error("images 폴더가 없어 images 폴더를 생성합니다");
+		fs.mkdirSync("images");
+	}
+});
+fs.readdir("images/uploads", (err) => {
+	if (err) {
+		console.error("uploads 폴더가 없어 uploads 폴더를 생성합니다");
+		fs.mkdirSync("images/uploads");
+	}
+});
+
+
+const form =
+    "<!DOCTYPE HTML><html><body>" +
+    "<form method='post' action='/upload' enctype='multipart/form-data'>" +
+    "<input type='file' name='upload'/>" +
+    "<input type='submit' /></form>" +
+    "</body></html>";
+
+
 // multer setting
 const upload = multer({
   storage: multer.diskStorage({
     // set a localstorage destination
     destination: (req, file, cb) => {
-      cb(null, '../images/uploads/');
+      cb(null, 'images/uploads/');
     },
     // convert a file name
     filename: (req, file, cb) => {
-      cb(null, new Date().valueOf() + path.extname(file.originalname));
+      cb(null, new Date().valueOf() +file.originalname);
     },
   }),
 });
 
-// upload image
-router.post("/upload", (req, res, next) => {
-  upload(req, res, function(err){
-    if( err instanceof multer.MulterError){
-      return next(err);
-    }else if (err){
-      return next(err);
-    }
-    console.log(req.file.originalname)
-    console.log(req.file.filename)
-    console.log(req.file.size)
 
-    return res.json({success:1, filename:req.file.filename})
-  });
+// upload image
+router.post("/upload", upload.single('file'), (req, res) => {
+	console.log(req.file);
 });
 
-// Find All
+// get image
 router.get('/', (req, res) => {
+	res.writeHead(StatusCode.OK, { "Content-Type": "text/html" });
+	res.end(form);
 });
 
 module.exports = router;
