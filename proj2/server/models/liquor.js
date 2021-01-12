c695f8b77743e090229a20f53168c379384f356b
Dajin Han <dajinstory@naver.com>
2021-01-12 10:32:35 +0000

    server

--- proj2/server/models/liquor.js
+++ proj2/server/models/liquor.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 // Define Schemes
 const liquorSchema = new mongoose.Schema({
-	_id:{type:Number, required:true, unique:true},
+	_id:{type:Number},
 	name:{type:String},
 	type:{type:String},
 	price:{type:Number},
@@ -11,7 +11,8 @@ const liquorSchema = new mongoose.Schema({
 	imgUri:{type:String}
 },
 {
-	timestamps:true
+	timestamps:false,
+	versionKey:false
 });
 
 // Create new document
@@ -25,20 +26,25 @@ liquorSchema.statics.findAll = function () {
   return this.find({});
 };
 
-// Find One by liquorid
-liquorSchema.statics.findOneByLiquorId = function (id) {
-  return this.findOne({ id });
+// Find One by Name
+liquorSchema.statics.findOneByName = function (name) {
+  return this.find({ name: name });
 };
 
-// Update by liquorAccountId
-liquorSchema.statics.updateByLiquorId = function (id, payload) {
+// Find One by Id
+liquorSchema.statics.findOneById = function (_id) {
+  return this.find({ _id: _id });
+};
+
+// Update by Id
+liquorSchema.statics.updateById = function (_id, payload) {
   // { new: true }: return the modified document rather than the original. defaults to false
-  return this.findOneAndUpdate({ id }, payload, { new: true });
+  return this.findOneAndUpdate({ _id: _id }, payload, { new: true });
 };
 
-// Delete by liquorAccountId
-liquorSchema.statics.deleteByLiquorId = function (id) {
-  return this.remove({ id });
+// Delete by Id
+liquorSchema.statics.deleteById = function (_id) {
+  return this.remove({ _id: _id });
 };
 
 // Create Model & Export
--- proj2/server/routes/liquors.js
+++ proj2/server/routes/liquors.js
@@ -3,20 +3,42 @@ const Liquor = require('../models/liquor');
 
 // Find All
 router.get('/', (req, res) => {
+  console.log("GET Liquor ALL");
   Liquor.findAll()
     .then((liquors) => {
+	  console.log("TRY");
       if (!liquors.length) return res.status(404).send({ err: 'Liquor not found' });
-      res.send(`find successfully: ${liquors}`);
+      console.log("SUCCESS");
+	  //res.send(`find successfully: ${liquors}`);
+	  res.json(liquors);
+    })
+    .catch(err => res.status(500).send(err));
+});
+
+// Find One by name
+router.get('/name/:name/', (req, res) => {
+  console.log("GET Liquor Single");
+  Liquor.findOneByName(req.params.name)
+    .then((liquor) => {
+      console.log("TRY");
+      if (!liquor) return res.status(404).send({ err: 'Liquor not found' });
+	  console.log("SUCCESS");
+	  //res.send(`findOne successfully: ${liquor}`);
+      res.json(liquor);
     })
     .catch(err => res.status(500).send(err));
 });
 
 // Find One by id
-router.get('/liquorid/:liquorid', (req, res) => {
-  Liquor.findOneByLiquorId(req.params.liquorid)
+router.get('/id/:_id/', (req, res) => {
+  console.log("GET Liquor Single");
+  Liquor.findOneById(req.params._id)
     .then((liquor) => {
+      console.log("TRY");
       if (!liquor) return res.status(404).send({ err: 'Liquor not found' });
-      res.send(`findOne successfully: ${liquor}`);
+	  console.log("SUCCESS");
+	  //res.send(`findOne successfully: ${liquor}`);
+      res.json(liquor);
     })
     .catch(err => res.status(500).send(err));
 });
@@ -29,15 +51,15 @@ router.post('/', (req, res) => {
 });
 
 // Update by id
-router.put('/liquorid/:liquorid', (req, res) => {
-  Liquor.updateByLiquorId(req.params.liquorid, req.body)
+router.put('/id/:_id/', (req, res) => {
+  Liquor.updateById(req.params._id, req.body)
     .then(liquor => res.send(liquor))
     .catch(err => res.status(500).send(err));
 });
 
 // Delete by id
-router.delete('/liquorid/:liquorid', (req, res) => {
-  Liquor.deleteByLiquorId(req.params.liquorid)
+router.delete('/id/:_id/', (req, res) => {
+  Liquor.deleteById(req.params._id)
     .then(() => res.sendStatus(200))
     .catch(err => res.status(500).send(err));
 });
